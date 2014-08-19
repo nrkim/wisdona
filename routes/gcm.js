09136fde581169caf15ab8d75dbfc3056a3be7eb
onam <nicekon@gmail.com>
2014-08-19 15:32:49 +0900

    GCM추가

--- routes/gcm.js
+++ routes/gcm.js
@@ -1,56 +1,104 @@
 /**
  * Created by onam on 2014. 8. 11..
  */
+var async = require('async');
+var gcmConfig = require('./config/gcm');
+
+/*
     config/gcm.js------------------------------
     module.exports = {
     apikey: '님꺼'
 
 };
-
+*/
 
 
 routs/index.js--------------------------------
 var gcm = require('node-gcm'),
     gcmConfig = require('../config/gcm');
 
-function registerId(req, res){
+// 유저 Device Id 업데이트
+exports.registerDeviceId = function(user_id, userDeviceId, callback){
     connectionPool.getConnection(function(err, connection){
         if(err){
-            res.json({error: err});
+            callback(err);
         }else{
-            var updateSql = 'UPDATE users SET gcm_registration_id = ? WHERE id = ?';
-            var registrationId = req.body.registrationId;
-            connection.query(updateSql, [registrationId, req.user.id], function(err, result){
+            var query = 'UPDATE users SET gcm_registration_id = ? WHERE user_id = ?';
+            connection.query(query, [userDeviceId, user.id], function(err, result){
                 if(err){
                     connection.release();
-                    res.json({error: err});
+                    callback(err);
                 }else{
                     connection.release();
-                    res.json({result: "Registration successful!!!"});
+                    callback();
                 }
             });
         }
     });
 }
 
-exports.sendMessage = function (req, res) {
+// 유저 Device Id 가져오기
+function getUserDeviceID(user_id,  callback) {
+    connectionPool.getConnection(function(err, connection){
+        if(err){
+            callback(err);
+        }else{
+            var query = 'SELECT gcm_registration_id FROM users WHERE user_id = ?';
+            connection.query(query, [user.id], function(err, rows, fields){
+                if(err){
+                    connection.release();
+                    callback(err);
+                }else{
+                    connection.release();
+                    if ( rows.length ){
+                        callback(null, rows[0].gcm_registration_id);
+                    }else{
+                        callback(new Error('해당 사용자가 없습니다.'));
+                    }
+                }
+            });
+        }
+    });
+}
 
-};
-function sendMessage(req, res){
+// GCM 보내기
+exports.sendMessage = function (users, title, message, callback ) {
     var message = new gcm.Message();
-    message.addDataWithKeyValue('Key1', 'message1');
-    message.addDataWithKeyValue('Key2', 'message2');
-    message.collapseKey = 'demo';
+    message.addDataWithKeyValue('title', title);
+    message.addDataWithKeyValue('message', message);
+    message.collapseKey = 'wisdona';
     message.delayWhileIdle = true;
     message.timeToLive = 3;
     message.dryRun = true;
 
     var sender = new gcm.Sender(gcmConfig.apikey);
     var registrationIds = [];
-    registrationIds.push('regId1');
-    registrationIds.push('regId2');
 
-    sender.send(message, registrationIds, 4, function(err, result){
-        console.log(result);
-    });
-}
+    // 디바이스 아이디 등록
+    async.each(users,
+        function (user_id, cb) {
+            var userDeviceId = getUserDeviceID(user_id, function (err, userDeviceId) {
+                if (err){
+                    // 에러처리
+                    cb(err);
+                }else{
+                    registrationIds.push(userDeviceId);
+                    cb();
+                }
+            });
+        },
+        function (err) {
+            if(err){
+                callback(err);
+            }else{
+                sender.send(message, registrationIds, 4, function(err, result){
+                    if (err){
+                        callback(err);
+                    }else{
+                        callback();
+                    }
+                });
+            }
+        }
+    );
+};
